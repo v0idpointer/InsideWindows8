3fd3d0ca48508e50e30672799325d10d9ce6b420
v0idpointer <akovacevic03@outlook.com>
2025-07-25 03:05:04 +0200

    Added immersive window demo app.

---
+++ 6.1.7779.0.fbl_grfx_dev1.100713-1700/ImmersiveWindow/Src/Main.cpp
@@ -0,0 +1,70 @@
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+constexpr const wchar_t* CLASS_NAME = L"ImmersiveWindowDemoApp";
+
+typedef HWND (WINAPI *CreateImmersiveWindow_t)(
+	DWORD dwExStyle,
+	LPCWSTR lpClassName,
+	DWORD dwStyle,
+	HWND hWndParent,
+	HINSTANCE hInstance,
+	LPVOID lpParam
+);
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+
+	if (msg == WM_CLOSE) {
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	return DefWindowProcW(hWnd, msg, wParam, lParam);
+}
+
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int nCmdShow) {
+
+	WNDCLASSEXW wc = { 0 };
+	wc.cbSize = sizeof(wc);
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hCursor = LoadCursorW(nullptr, IDC_ARROW);
+	wc.hIcon = LoadIconW(nullptr, IDI_APPLICATION);
+	wc.hIconSm = LoadIconW(nullptr, IDI_APPLICATION);
+	wc.hbrBackground = CreateSolidBrush(RGB(255, 255, 255));
+	wc.lpszClassName = CLASS_NAME;
+	wc.lpszMenuName = nullptr;
+	wc.lpfnWndProc = &WindowProc;
+	wc.style = (CS_OWNDC | CS_VREDRAW | CS_HREDRAW);
+
+	if (RegisterClassExW(&wc) == NULL) {
+		MessageBoxW(nullptr, L"Failed to register window class.", L"Error", MB_ICONERROR);
+		return -1;
+	}
+
+	HMODULE hLib = LoadLibraryW(L"twinapi.dll");
+	if (hLib == nullptr) {
+		MessageBoxW(nullptr, L"Failed to load twinapi.dll", L"Error", MB_ICONERROR);
+		return -1;
+	}
+
+	CreateImmersiveWindow_t pfnCreateImmersiveWindow = reinterpret_cast<CreateImmersiveWindow_t>(
+		GetProcAddress(hLib, "CreateImmersiveWindow")
+	);
+
+	if (pfnCreateImmersiveWindow == nullptr) {
+		MessageBoxW(nullptr, L"TWinAPI does not export CreateImmersiveWindow function.", L"Error", MB_ICONERROR);
+		return -1;
+	}
+
+	pfnCreateImmersiveWindow(NULL, CLASS_NAME, WS_VISIBLE, nullptr, hInstance, nullptr);
+
+	MSG msg = { 0 };
+	while (GetMessageW(&msg, nullptr, 0, 0)) {
+		TranslateMessage(&msg);
+		DispatchMessageW(&msg);
+	}
+
+	return 0;
+}
--- 6.1.7779.0.fbl_grfx_dev1.100713-1700/projects.lua
+++ 6.1.7779.0.fbl_grfx_dev1.100713-1700/projects.lua
@@ -1,2 +1,3 @@
 group ("6.1.7779.0.fbl_grfx_dev1.100713-1700")
     include "CompanionWindow/premake5.lua"
+    include "ImmersiveWindow/premake5.lua"
